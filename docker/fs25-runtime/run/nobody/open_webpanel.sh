ecf12faaba9f936184b169fa7a414f157c894ed6
DaVaR <davarravad@users.noreply.github.com>
2026-03-15 11:12:06 -0500

    updates

--- docker/fs25-runtime/run/nobody/open_webpanel.sh
+++ docker/fs25-runtime/run/nobody/open_webpanel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-WEB_PORT_VALUE="${SERVER_PORT:-7999}"
+WEB_PORT_VALUE="${WEB_PORT:-${SERVER_PORT:-7999}}"
 
 exec firefox "http://127.0.0.1:${WEB_PORT_VALUE}/index.html?lang=en&username=${WEB_USERNAME}&password=${WEB_PASSWORD}&login=Login"
