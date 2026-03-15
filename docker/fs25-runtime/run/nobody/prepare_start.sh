00fe721cf641c6a659066851c617ca5eadd4366d
DaVaR <davarravad@users.noreply.github.com>
2026-03-15 12:32:40 -0500

    Updates

--- docker/fs25-runtime/run/nobody/prepare_start.sh
+++ docker/fs25-runtime/run/nobody/prepare_start.sh
@@ -2,14 +2,14 @@
 
 . /usr/local/bin/runtime_log.sh
 . /usr/local/bin/wine_init.sh
-runtime_log_write "[Fragify Daemon]: Updating process configuration files..."
+runtime_log_write "[FSG Daemon]: Updating process configuration files..."
 
 . /usr/local/bin/wine_symlinks.sh
 . /usr/local/bin/fs25_common.sh
-runtime_log_write "[Fragify Daemon]: Ensuring file permissions are set correctly, this could take a few seconds..."
+runtime_log_write "[FSG Daemon]: Ensuring file permissions are set correctly, this could take a few seconds..."
 ensure_runtime_directories
 
 . /usr/local/bin/copy_server_config.sh
-runtime_log_write "[Fragify Daemon]: Checking server disk space usage, this could take a few seconds..."
+runtime_log_write "[FSG Daemon]: Checking server disk space usage, this could take a few seconds..."
 
 . /usr/local/bin/cleanup_logs.sh
